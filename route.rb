07fcb081e24b9707374ee925c451315f38c885ec
jiangguimin <jiangguimin@shensen.magus-soft.com>
2013-06-19 11:25:19 +0800

    ground namespace

--- route.rb
+++ route.rb
@@ -1,16 +1,18 @@
-# session
-get   '/sign_up', Session::SignForm
-post  '/sign_up', Session::SignUp
-post  '/login',   Session::Login
-get   '/login',   Session::LoginForm
-match '/logout',  Session::Logout
+Ground do
+  # session
+  get   '/sign_up', Session::SignForm
+  post  '/sign_up', Session::SignUp
+  post  '/login',   Session::Login
+  get   '/login',   Session::LoginForm
+  match '/logout',  Session::Logout
 
-# scene
-get  '/scenes',    Scene::Index
-post '/scene',     Scene::Create
-get  '/scene/new', Scene::New
-get  '/scene/:id', Scene::Show
+  # scene
+  get  '/scenes',    Scene::Index
+  post '/scene',     Scene::Create
+  get  '/scene/new', Scene::New
+  get  '/scene/:id', Scene::Show
 
-# activity
-post '/activity', Activity::Create
+  # activity
+  post '/activity', Activity::Create
 
+end
