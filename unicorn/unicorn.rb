1c155826e67cfbba777e9ef7dc0f14a90f0636c6
kayakjiang <kayak.jiang@gmail.com>
2013-07-10 20:41:22 +0800

    unicorn support

--- unicorn/unicorn.rb
+++ unicorn/unicorn.rb
@@ -1,17 +1,17 @@
 # set path to app that will be used to configure unicorn,
 # note the trailing slash in this example
-@dir = "/Users/jim/Projects/SceneMaster"
+app_dir = File.expand_path('../', File.dirname(__FILE__))
 worker_processes 2
-working_directory @dir
+working_directory app_dir
 preload_app true
 timeout 30
 
 # Specify path to socket unicorn listens to,
 # we will use this in our nginx.conf later
-listen "#{@dir}/tmp/sockets/unicorn.sock", :backlog => 64
+listen "#{app_dir}/tmp/sockets/unicorn.sock", :backlog => 64
 # Set process id path
-pid "#{@dir}/tmp/pids/unicorn.pid"
+pid "#{app_dir}/tmp/pids/unicorn.pid"
 
 # Set log file paths
-stderr_path "#{@dir}/logs/unicorn.stderr.log"
-stdout_path "#{@dir}/logs/unicorn.stdout.log" 
+stderr_path "#{app_dir}/logs/unicorn.stderr.log"
+stdout_path "#{app_dir}/logs/unicorn.stdout.log" 
